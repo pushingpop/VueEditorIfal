9779ea5709f4942c5d8079b9696a59cf602872b9
Raphael Dantas Cavalcante <rdccac@gmail.com>
2018-03-09 17:35:16 -0300

    Commit

--- src/App.vue
+++ src/App.vue
@@ -48,9 +48,53 @@
       </b-modal>
     </div>
 
+    <div>
+      <!-- Modal Component -->
+      <b-modal id="modalPrevent"
+               ref="modaltable"
+               title="Dados da Tabela"
+               @ok="handleOkTable"
+               @shown="clearTable">
+        <form @submit.stop.prevent="handleSubmitTable">
+          <b-form-input type="text"
+            placeholder="Titulo: "
+            v-model="globaltitulo">
+          </b-form-input>
+          <p></p>
+          <div>
+            <b-form-file  v-model="file" ref="fileinputTable" class="mt-3" plain :accept="SheetJSFT" @change="_change"></b-form-file>
+          </div>
+          <p></p>
+          <b-form-input type="text"
+            placeholder="Fonte: "
+            v-model="globalfonte">
+          </b-form-input>
+          <p></p>
+          <b-form-input type="text"
+            placeholder="Informações adicionais: "
+            v-model="globalinfos">
+          </b-form-input>
+          <p></p>
+          <b>
+          </b>
+        </form>
+      </b-modal>
+    </div>
+
+    <div id="toolbar">
+      <!-- Add font size dropdown -->
+      <button id="custom-image" ><svg viewBox="0 0 18 18"> <rect class="ql-stroke" height="10" width="12" x="3" y="4"></rect> <circle class="ql-fill" cx="6" cy="7" r="1"></circle> <polyline class="ql-even ql-fill" points="5 12 5 11 7 9 8 10 11 7 13 9 13 12 5 12"></polyline> </svg></button>
+      <button class="ql-bold"></button>
+      <button class="ql-italic"></button>
+      <button id="custom-table" ><svg viewBox="0 0 18 18"> <rect class="ql-stroke" height="10" width="12" x="3" y="4"></rect> <circle class="ql-fill" cx="6" cy="7" r="1"></circle> <polyline class="ql-even ql-fill" points="5 12 5 11 7 9 8 10 11 7 13 9 13 12 5 12"></polyline> </svg></button>
+
+    </div>
+
     <vue-editor id="editor"
       useCustomImageHandler
       @imageAdded="handleImageAdded"
+      :editorOptions="editorSettings"
+
       v-model="editorContent">
     </vue-editor>
   </div>
@@ -60,16 +104,40 @@
 <script>
 import {VueEditor, Quill} from 'vue2-editor'
 import axios from 'axios'
+import XLSX from '../node_modules/xlsx';
+import HotTable from '../node_modules/vue-handsontable-official';
+const make_cols = refstr => Array(XLSX.utils.decode_range(refstr).e.c + 1).fill(0).map((x,i) => ({name:XLSX.utils.encode_col(i), key:i}));
+const _SheetJSFT = [
+	"xlsx", "xlsb", "xlsm", "xls", "xml", "csv", "txt", "ods", "fods", "uos", "sylk", "dif", "dbf", "prn", "qpw", "123", "wb*", "wq*", "html", "htm"
+].map(function(x) { return "." + x; }).join(",");
+
 
 export default {
   name: 'app',
   components: {
-    VueEditor,
+    VueEditor,HotTable,
 
   },
   data () {
     return {
+      SheetJSFT: _SheetJSFT,
+      root: 'test-hot',
+      hotSettings:
+      {
+        data: [
+
+        ],
+        //colHeaders: true,
+        colHeaders: "",
+        editor: false
+      },
+
       editorContent: '<p><p contenteditable="false" class="fig" draggable="true" id="0" Ondrag><a class="atexto">[Fig 23]</a><a href="#" class="delete";">Delete</a></p></p><p>um outro paragrafo</p><p>um outro paragrafo</p><p>um outro paragrafo</p><p><p contenteditable="false" class="fig" draggable="true" id="1">[Fig 2]<a href="#" class="delete";">Delete</a></p></p><p>Test test 123</p>',
+      editorSettings: {
+        modules: {
+          toolbar: '#toolbar'
+        }
+      },
           fotos: [
             {
               url: 'https://dummyimage.com/800x800/000/fff&text=Fig1',
@@ -89,16 +157,24 @@ export default {
             }
           ],
           globalID: 1,
+          tableID: 0,
           editorFotos: [],
           varFotos: [],
           element: null,
           figs: [],
           figuras: [],
           file: null,
-          globalurl: ''
+          globalurl: '',
+
+          globaltabela: [],
+          globaltag: '',
+          globaltitulo: '',
+          globalfonte: '',
+          globalinfos: ''
 
 
     }
+
   },
 
 
@@ -155,20 +231,33 @@ export default {
       document.getElementById('editor').getElementsByTagName('p')[i].addEventListener('dragenter', this.dragenter_handler, false);
     }
     var self = this;
-    var customButton = document.querySelector('.ql-image');
-    customButton.addEventListener('click', function() {
-    var modal1 = document.getElementById("modalPrevent");
-    self.showModal();
+
+    var customImage = document.querySelector('#custom-image');
+      customImage.addEventListener('click', function() {
+      self.showModal();
+    });
+
+    var customTable = document.querySelector('#custom-table');
+      customTable.addEventListener('click', function() {
+      self.showModalTable();
     });
   },
 
   methods: {
 
-    showModal () {
-      this.$refs.modal.show()
+    customFormulaHandler(){
+      console.log("Alerta");
     },
 
-    clearName () {
+    showModal () {
+      this.$refs.modal.show();
+    },
+
+    showModalTable () {
+      this.$refs.modaltable.show();
+    },
+
+    clearName() {
   this.fotos.url = '';
   this.fotos.titulo = '';
   this.fotos.fonte= '';
@@ -176,6 +265,48 @@ export default {
   this.$refs.fileinput.reset();
   this.globalurl = '';
 },
+
+  clearTable(){
+    this.globaltabela = [];
+    this.globaltitulo = '';
+    this.globalfonte = '';
+    this.globalinfos = '';
+    this.$refs.fileinputTable.reset();
+  },
+
+  handleOkTable (evt) {
+    // Prevent modal from closing
+    evt.preventDefault()
+    if (!this.globaltitulo) {
+      alert('Por favor, digite o título da tabela');
+    }
+    if(!this.globalfonte){
+      alert('Por favor, digite a fonte da tabela');
+    }
+    if(!this.file){
+      alert('Por favor, coloque uma tabela');
+    }
+    if(this.globaltitulo && this.globalfonte && this.file)
+    {
+      this.handleSubmitTable(this.globaltitulo, this.globaltabela, this.globalfonte, this.globalinfos)
+    }
+
+  },
+
+  handleSubmitTable (titulo, tabela, fonte, infos) {
+    this.hotSettings.data.push({
+      titulo: titulo,
+      tabela: this.globaltabela,
+      nome: "Tabela",
+      tag: '',
+      fonte: fonte,
+      infos: infos,
+    })
+    this.clearTable();
+    this.$refs.modaltable.hide();
+  },
+
+
 handleOk (evt) {
   // Prevent modal from closing
   evt.preventDefault()
@@ -210,13 +341,13 @@ handleSubmit (url, titulo, fonte, infos) {
     id: this.globalID.toString(),
     titulo: titulo,
     fonte: fonte,
-    infos: infos
+    infos: infos,
   })
   this.figsCount();
 
   //this.fotos.url='', this.fotos.nome='', this.fotos.id='', this.fotos.titulo, this.fotos.fonte='', this.fotos.indos='')
-  this.clearName()
-  this.$refs.modal.hide()
+  this.clearName();
+  this.$refs.modal.hide();
 },
 
 
@@ -305,49 +436,52 @@ arrayvarFotos(){
 
     handlertest(e){
       var files = e.target.files;
-
+      var self = this;
       if (files[0]) {
-	var img = new Image()
+	       var img = new Image()
+      	img.addEventListener('load', function () {
+          console.log(this.width + 'x' + this.height)
+          if(this.width > 900){
+            self.$refs.fileinput.reset();
+            alert("Imagem com largura maior que 900px");
+          }
+          else
+          {
+            const CLIENT_ID = '993793b1d8d3e2e'
+            var formData = new FormData();
+            formData.append('image', files[0])
 
-	img.addEventListener('load', function () {
-		console.log(this.width + 'x' + this.height)
-	})
+            axios({
+              url: 'https://api.imgur.com/3/image',
+              method: 'POST',
+              headers:{
+                'Authorization': 'Client-ID ' + CLIENT_ID
+              },
+              data: formData
+            })
+            .then((result) => {
+              console.log(result);
+              let url = result.data.data.link
+              self.globalurl = url;
+              //Editor.insertEmbed(cursorLocation, 'image', url);
 
-	img.setAttribute('src', URL.createObjectURL(files[0]))
-}
+              //Editor.insertEmbed(cursorLocation, 'image', url); // inserir no final
+              self.globalID++
+              console.log(url);
+              console.log(this.globalurl + " Global URL");
+              //}
+              //console.log(this.fotos[0]);
 
-      if(files.width > 900){
-        alert("Imagem com largura maior que 900px");
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+          }
+      	})
+      	img.setAttribute('src', URL.createObjectURL(files[0]))
       }
-      const CLIENT_ID = '993793b1d8d3e2e'
-      var formData = new FormData();
-      formData.append('image', files[0])
 
-      axios({
-        url: 'https://api.imgur.com/3/image',
-        method: 'POST',
-        headers:{
-          'Authorization': 'Client-ID ' + CLIENT_ID
-        },
-        data: formData
-      })
-      .then((result) => {
-        console.log(result);
-        let url = result.data.data.link
-        this.globalurl = url;
-        //Editor.insertEmbed(cursorLocation, 'image', url);
 
-        //Editor.insertEmbed(cursorLocation, 'image', url); // inserir no final
-        this.globalID++
-        console.log(url);
-
-        //}
-        //console.log(this.fotos[0]);
-
-      })
-      .catch((err) => {
-        console.log(err);
-      })
     },
 
     handleImageAdded(file, Editor, cursorLocation) {
@@ -405,7 +539,55 @@ arrayvarFotos(){
     var content = document.getElementById(contentId);
     // only one image, just return an item; or you can return an array
     if (content) return document.getElementsByTagName('img')[0];
+},
+
+/* Table */
+_suppress(evt) { evt.stopPropagation(); evt.preventDefault(); },
+_drop(evt) {
+  evt.stopPropagation(); evt.preventDefault();
+  const files = evt.dataTransfer.files;
+  if(files && files[0]) this._file(files[0]);
+},
+_change(evt) {
+  const files = evt.target.files;
+  if(files && files[0]) this._file(files[0]);
+},
+_export(evt) {
+  /* convert state to workbook */
+  const ws = XLSX.utils.aoa_to_sheet(this.hotSettings.data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+  /* generate file and send to client */
+  XLSX.writeFile(wb, "sheetjs.xlsx");
+},
+_file(file) {
+  /* Boilerplate to set up FileReader */
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    /* Parse data */
+    const bstr = e.target.result;
+    const wb = XLSX.read(bstr, {type:'binary'});
+    /* Get first worksheet */
+    const wsname = wb.SheetNames[0];
+    const ws = wb.Sheets[wsname];
+    /* Convert array of arrays */
+    const data = XLSX.utils.sheet_to_json(ws, {header:1});
+    /* Nova data*/
+    console.log(file);
+    /* Update state */
+    //this.hotSettings.data.tabela = data;
+    this.globaltabela = data;
+    //this.hotSettings.colHeaders = data[0];
+    //var matches = document.getElementsByTagName("tr");
+    //matches[0].style.visibility = "collapse";
+    //this.hotSettings.colHeaders = data[0];
+    //console.log(matches);
+    this.cols = make_cols(ws['!ref']);
+    //console.log(this.hotSettings.data);
+  };
+  reader.readAsBinaryString(file);
 }
+
   }
 }
 </script>
